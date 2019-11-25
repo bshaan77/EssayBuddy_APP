4259da9cc8218012a381b94d7f143cbeaeb62639
IshaanBansal <ishaanbansal1001@gmail.com>
2019-11-25 12:32:35 -0800

    Create ViewController.swift

---
+++ EssayBuddy7.0/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  EssayBuddy7.0
+//
+//  Created by Ishaan Bansal on 11/24/19.
+//  Copyright © 2019 Ishaan Bansal. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    //@IBOutlet weak var WebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //let url = URL(string: "http://schoolloop.ga/essaybuddy")
+        //let request = URLRequest(url: url!)
+        
+        //WebView.load(request)
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
